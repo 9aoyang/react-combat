c45ab5ffbdfe3f5bf6f7628ce198bcccd89ce257
9aoyang <9aoyang@gmail.com>
2017-10-31 10:58:06 +0800

    save Comments in localStorage

--- src/CommentApp.js
+++ src/CommentApp.js
@@ -10,13 +10,30 @@ class CommentApp extends Component {
     }
   }
 
-  handleSubmitComment (comment) {
-    // console.log(comment)
-    this.state.comments.push(comment)
-    this.setState({
-      comments: this.state.comments
-    })
+  componentWillMount () {
+    this._loadComments()
+  }
 
+  _loadComments () {
+    let comments = localStorage.getItem('comments')
+    if (comments) {
+      comments = JSON.parse(comments)
+      this.setState({comments})
+    }
+  }
+
+  _savaComments (comments) {
+    localStorage.setItem('comments', JSON.stringify(comments))
+  }
+
+  handleSubmitComment (comment) {
+    if(!comment) return
+    if(!comment.username) alert('请输入用户名！')
+    if(!comment.content) alert('请输入评论内容！')
+    const comments = this.state.comments
+    comments.push(comment)
+    this.setState({comments})
+    this._savaComments(comments)
   }
 
   render () {
