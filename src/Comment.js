d791ed6a88b7af93910966fe901bd6d6c60d9d33
9aoyang <9aoyang@gmail.com>
2017-10-31 11:25:12 +0800

    add comment timer

--- src/Comment.js
+++ src/Comment.js
@@ -1,6 +1,34 @@
 import React, {Component} from 'react'
-
+import ProtTypes from 'prop-types'
 class Comment extends Component {
+  static protTypes = {
+    comment: ProtTypes.object.isRequired
+  }
+
+  constructor () {
+    super()
+    this.state = { timeString: '' }
+  }
+
+  componentWillMount () {
+    this._updateTimeString()
+    this._timer = setInterval (
+      () => this._updateTimeString(),
+      5000
+    )
+  }
+
+  _updateTimeString () {
+    const comment = this.props.comment
+    const duration = (+Date.now() - comment.createTime) / 1000
+    this.setState({
+      timeString: duration > 60
+      ? `${Math.round(duration / 60)} 分钟前`
+      : `${Math.round(Math.max(duration, 1))} 秒前`
+    })
+  }
+
+
   render() {
     return(
       <div className='comment'>
@@ -8,6 +36,9 @@ class Comment extends Component {
           <span>{this.props.comment.username} </span>：
         </div>
         <p>{this.props.comment.content}</p>
+        <span className='comment-createdtime'>
+          {this.state.timeString}
+        </span>
       </div>
     )
   }
